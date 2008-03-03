138dfb5d7bbd08037e3d7c495f7f2cf42d7324f0
Marcus Ahnve <marcus@ahnve.com>
2008-03-03 23:44:56 +0100

    refactored js

--- public/scripts/signup.js
+++ public/scripts/signup.js
@@ -1,19 +1,17 @@
 $(document).ready(function() {
 
-  if($('#attendant_wants_to_speak').attr('checked')) {
-    $('#speakingproposal').show('slow')
-  } else {
-    $('#speakingproposal').hide('slow')      
-  }
-
+  toggleCheckBox();
   
   $('#attendant_wants_to_speak').click(function() {
-    if($(this).attr('checked')) {
+    toggleCheckBox();
+  });
+
+
+  function toggleCheckBox() {
+    if($('#attendant_wants_to_speak').attr('checked')) {
       $('#speakingproposal').show('slow')
     } else {
       $('#speakingproposal').hide('slow')      
-    }
-  });
-
-  
+    }    
+  }
 });
