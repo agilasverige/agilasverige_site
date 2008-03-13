86d47419e2aa9a020e28c2705dda300150011034
Marcus Ahnve <marcus@ahnve.com>
2008-03-13 20:48:19 +0100

    added git options to deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,26 +1,31 @@
-set :application, "agilasverige" 
-set :user, "???" 
-set :domain, "a" 
-set :repository, "svn+ssh://#{user}@#{domain}/var/www/apps/repos/#{application}/trunk" 
-set :deploy_to, "/var/www/apps/#{application}" 
+set :application, "agilasverige2008" 
+set :user, "agilesweden" 
+set :domain, "agilasverige.se" 
+set :repository, "git://github.com/mahnve/agilasverige.git" 
+set :deploy_to, "/home/agilesweden/apps/#{application}" 
 
 set :scm, "git"
+set :deploy_via, :remote_cache
+set :branch, "origin/master"
 
-role :app, domain
-role :web, domain
-role :db, domain, :primary => true
+role :app, "stjernstrom.se"
+role :web, "stjernstrom.se"
+role :db, "stjernstrom.se", :primary => true
+
+default_run_options[:pty] = true
+
+ssh_options[:port] = 22666
+
 
 desc "Link in the production extras" 
 task :after_symlink do
     # symlink log path
   run "ln -nfs #{shared_path}/log #{release_path}/log" 
-    # symlink path to database, this is only needed if you are using sqlite (which is ok for little things and is really easy)
-  run "ln -nfs #{shared_path}/db/#{application}_production.sqlite3 #{release_path}/db/#{application}_production.sqlite3" 
 end
 
 desc "Merb it up" 
 task :restart do
   run "cd #{current_path};./script/stop_merb" 
   # run "cd #{current_path};env EVENT=1 merb -e production -c 1" # for evented mongrel
-  run "cd #{current_path}; merb -e production -c 1" # plain old mongrel
+  run "cd #{current_path}; merb -e production -c 3 -p 12000" # plain old mongrel
 end
