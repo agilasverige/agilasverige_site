dc9e9b585a71015f6c9214685dc9656c5868033f
Marcus Ahnve <marcus@ahnve.com>
2008-05-22 23:48:50 +0200

    caching

--- app/controllers/program.rb
+++ app/controllers/program.rb
@@ -1,4 +1,6 @@
 class Program < Application
+
+  cache_page :index
   
   def index
     @schedule = SpeakerSchedule.new
--- config/init.rb
+++ config/init.rb
@@ -36,6 +36,7 @@ dependencies "haml", "merb-haml"
 dependency "merb_helpers"
 dependency "merb_stories"
 dependency "merb-assets"
+dependency "merb-cache" , "=0.9.2"
 
 require 'merb_has_flash'
 require "merb-mailer"
