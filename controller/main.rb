5864ccad8df0117a9cad67a19efe8bda6042c224
Marcus Ahnve <marcus@ahnve.com>
2008-10-21 08:54:05 +0200

    cleaned up, added first bit of Erector

--- controller/main.rb
+++ controller/main.rb
@@ -1,18 +1,30 @@
 class MainController < Ramaze::Controller
 
-  engine :Haml
-  layout '/layout'
+  # engine :Haml
+  # layout '/layout'
   
-  deny_layout :sponsors
+  # deny_layout :sponsors
+
   def index
+    MainView.new.to_s
   end
   
   def thanks
   end
   
   def program
+    @schedule = SpeakerSchedule.new
   end
 
   def sponsors
   end
+
+  def talkinfo
+    @talkinfo = {:day => request[:day],
+                 :track => request[:track],
+                 :slot => request[:slot],
+                 :talk => request[:talk]}
+    
+  end
+
 end
--- start.rb
+++ start.rb
@@ -1,9 +1,31 @@
+#!/usr/bin/env ruby
 require 'rubygems'
 require 'ramaze'
 
 # require all controllers and models
 acquire __DIR__/:controller/'*'
 acquire __DIR__/:lib/'*'
+acquire __DIR__/:view/'*.rb'
 # acquire __DIR__/:model/'*'
 
+# Ramaze::Route[/(\/2008)\/(.*)/] = "Vjjjjjjj1
+
+  
+# r.match("/2008") do |year| 
+#   year.match('').to(:controller => 'articles', :action => 'index').name(:home)
+#   year.match('/tack').to(:controller => 'articles', :action => 'thanks').name(:thanks_for_signing_up)
+#   year.match('/program').to(:controller => 'program', :action => 'index').name(:program)
+#   year.match('/all_emails').to(:controller => 'attendants', :action => 'all_emails').name(:all_emails)
+#   year.match('/speaker_emails').to(:controller => 'attendants', :action => 'speaker_emails').name(:speaker_emails)
+# 
+#   year.resources :attendants do |a|
+#     a.resources :speaking_proposals
+#   end
+#
+#   year.resources :speaking_proposals
+# end
+# 
+# r.match("/").to(:controller => 'articles', :action => 'index')
+ 
+  
 Ramaze.start :adapter => :mongrel, :port => 7000
