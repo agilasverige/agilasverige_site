3b4e4d795367404fad1e77216dc99c42b7e5159f
Marcus Ahnve <marcus@ahnve.com>
2009-02-21 23:56:14 +0100

    vladding

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,7 @@ set :repository, "git://github.com/mahnve/agilasverige.git"
 set :deploy_to, "/home/agilesweden/apps/#{application}" 
 
 set :branch, "master"
+set :web, "nginx"
 
 
 #default_run_options[:pty] = true
@@ -15,19 +16,21 @@ set :ssh_flags,             '-p 22666'
 desc "Link in the production extras" 
 namespace :vlad do
   remote_task :after_symlink do
-    # symlink log path
-    run "ln -nfs #{shared_path}/log #{release_path}/log" 
+    run "ln -nfs #{shared_path}/log #{current_path}/log" 
   end
 
-  desc "Merb it up" 
-  remote_task :restart do
-    run "cd #{current_path};merb -K all" 
-    # run "cd #{current_path};env EVENT=1 merb -e production -c 1" # for evented mongrel
-    run "cd #{current_path}; merb -e production -c 3 -p 12000" # plain old mongrel
+  desc "Start it up" 
+  remote_task :start_app do
+    run "cd #{current_path};./start.sh" 
   end
-  
-  
+
+  desc "Tear it down" 
+  remote_task :stop_app do
+    run "cd #{current_path};./stop.sh" 
+  end
+
 end
+remote_task :deploy => ["vlad:update", "vlad:stop_app", "vlad:start_app"]
 
 remote_task :copy_database_yml => 'deploy:update_code' do
   run "cd #{release_path} && cp ../../db.yml config/database.yml"
