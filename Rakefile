27e9502c338e0d65c91083086eb89c13ee5070d9
Marcus Ahnve <marcus@ahnve.com>
2009-02-22 20:06:41 +0100

    fixed default mongrel start

--- Rakefile
+++ Rakefile
@@ -1,6 +1,6 @@
 begin
   require 'vlad'
-  Vlad.load :scm => 'git'
+  Vlad.load :scm => 'git', :app => nil
 rescue LoadError
   # do nothing
 end
--- config/deploy.rb
+++ config/deploy.rb
@@ -12,9 +12,31 @@ set :web, "nginx"
 
 set :ssh_flags,             '-p 22666'
 
-
-desc "Link in the production extras" 
 namespace :vlad do
+  ##
+  # ramaze app server
+
+  set :ramaze_address,       "127.0.0.1"
+  set :ramaze_clean,         false
+  set :ramaze_command,       'ramaze'
+  set(:ramaze_conf)          nil
+  set :ramaze_config_script, nil
+  set :ramaze_environment,   "live"
+  set :ramaze_group,         nil
+  set :ramaze_log_file,      nil
+  set :ramaze_pid_file,      nil
+  set :ramaze_port,          7000
+  set :ramaze_prefix,        nil
+  set :ramaze_servers,       3
+  set :ramaze_user,          nil
+
+  desc "Prepares application servers for deployment. ramaze
+configuration is set via the ramaze_* variables.".cleanup
+
+  remote_task :setup_app, :roles => :app do
+    'rake'
+  end
+
   remote_task :after_symlink do
     run "ln -nfs #{shared_path}/log #{current_path}/log" 
   end
@@ -30,9 +52,5 @@ namespace :vlad do
   end
 
 end
-remote_task :deploy => ["vlad:update", "vlad:stop_app", "vlad:start_app"]
 
-remote_task :copy_database_yml => 'deploy:update_code' do
-  run "cd #{release_path} && cp ../../db.yml config/database.yml"
-end
-  
+remote_task :deploy => ["vlad:update", "vlad:stop_app", "vlad:start_app"]
