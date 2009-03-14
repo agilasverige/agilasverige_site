cc60ffceecd3d63a13cd0f1d6fba6b9682074ef4
Marcus Ahnve <marcus@ahnve.com>
2009-03-14 22:59:34 +0100

    added a missing dot

--- view/index.rb
+++ view/index.rb
@@ -5,7 +5,7 @@ class MainView < BaseView
     p do
       text 'Förra årets mest uppskattade konferens om agil systemutveckling är tillbaka! I år går den av stapeln 8-9 juni på Ingenjörshuset i Stockholm.' 
     end
-    p 'Precis som förra året kommer konferensen att bestå av tio-minuters blixttal på förmiddagarna med uppföljande Open Space på eftermiddagarna'
+    p 'Precis som förra året kommer konferensen att bestå av tio-minuters blixttal på förmiddagarna med uppföljande Open Space på eftermiddagarna.'
     p 'Det kommer snart att vara möjligt att anmäla sig både som talare och deltagare. Precis som förra året kommer talare att gå gratis samtidigt som avgiften för deltagare är högst överkomlig.'
     p do 
       text 'Under tiden kan man - äntligen - ta del av '
