6e166a32cda3739792bfa03988a4035dc03b054f
Marcus Ahnve <marcus@ahnve.com>
2009-05-22 11:30:42 +0200

    cleaned up view

--- view/program.rb
+++ view/program.rb
@@ -21,20 +21,21 @@ module ProgramView
         end
         tr do
           td do 
-            '09:00 - 10:00'
+            text '09:00 - 10:00'
           end
           td  do
             p(:class => 'title') do
-              'Introduktion'
+              text 'Introduktion'
             end
           end
           td do
             p(:class => 'title') do 
-              'Introduktion'
+              text 'Introduktion'
             end
           end
         end
         tr do
+          td
           td  do
             talkinfo
           end
@@ -71,21 +72,12 @@ module ProgramView
         end
         tr do
           td '10:00 - 10:20'
-          td(:class => 'paus', :colspan => '2') do 'Kaffepaus' end
+          td(:class => 'paus', :colspan => '2') do text 'Kaffepaus' end
         end
         tr do
           td do
-            '10:20 - 11:20'
+            text '10:20 - 11:20'
           end
-          td do
-            talkinfo
-          end
-          td do
-            talkinfo
-          end
-        end
-        tr do
-          td do end
           td do
             talkinfo
           end
@@ -121,11 +113,20 @@ module ProgramView
           end
         end
         tr do
-          td do '11:20 - 11:30' end
-          td(:class => 'paus', :colspan => '2') do 'Paus' end
+          td do end
+          td do
+            talkinfo
+          end
+          td do
+            talkinfo
+          end
         end
         tr do
-          td do '11:30 - 12:30' end
+          td do text '11:20 - 11:30' end
+          td(:class => 'paus', :colspan => '2') do text 'Paus' end
+        end
+        tr do
+          td do text '11:30 - 12:30' end
           td do
             talkinfo
           end
@@ -170,24 +171,24 @@ module ProgramView
           end
         end
         tr do
-          td do '12:30 - 13:30' end
-          td(:class => 'paus', :colspan => '2') do 'Lunch' end
+          td do text '12:30 - 13:30' end
+          td(:class => 'paus', :colspan => '2') do text 'Lunch' end
         end
         tr do
-          td do '13:30 - 14:15' end
-          td(:class => 'paus', :colspan => '2') do  'Open Space inledning' end
+          td do text '13:30 - 14:15' end
+          td(:class => 'paus', :colspan => '2') do  text 'Open Space inledning' end
         end
         tr do
-          td do '14:15 - 15:00' end
-          td(:class => 'paus', :colspan => '2') do 'Open Space' end
+          td do text '14:15 - 15:00' end
+          td(:class => 'paus', :colspan => '2') do text 'Open Space' end
         end
         tr do
-          td do '15:15 - 16:00' end
-          td(:class => 'paus', :colspan => '2') do 'Open Space' end
+          td do text '15:15 - 16:00' end
+          td(:class => 'paus', :colspan => '2') do text 'Open Space' end
         end
         tr do
-          td do '16:15 - 17:00' end
-          td(:class => 'paus', :colspan => '2') do 'Open Space' end
+          td do text '16:15 - 17:00' end
+          td(:class => 'paus', :colspan => '2') do text 'Open Space' end
         end
       end
       h1 'Tisdag'
@@ -198,7 +199,7 @@ module ProgramView
           th 'Polhemssalen'
         end
         tr do
-          td do '09:00 - 10:00' end
+          td do text '09:00 - 10:00' end
           td  do
             talkinfo
           end
@@ -243,11 +244,11 @@ module ProgramView
           end
         end
         tr do
-          td do '10:00 - 10:20' end
-          td(:class => 'paus', :colspan => '2') do 'Kaffepaus' end
+          td do text '10:00 - 10:20' end
+          td(:class => 'paus', :colspan => '2') do text 'Kaffepaus' end
         end
         tr do
-          td do '10:20 - 11:20' end
+          td do text '10:20 - 11:20' end
           td  do
             talkinfo
           end
@@ -292,11 +293,11 @@ module ProgramView
           end
         end
         tr do
-          td do '11:20 - 11:30' end
-          td(:class => 'paus', :colspan => '2') do 'Paus' end
+          td do text '11:20 - 11:30' end
+          td(:class => 'paus', :colspan => '2') do text 'Paus' end
         end
         tr do
-          td do '11:30 - 12:30' end
+          td do text '11:30 - 12:30' end
           td  do
             talkinfo
           end
@@ -342,23 +343,23 @@ module ProgramView
       end
       tr do
         td '12:30 - 13:30'
-        td(:class => 'paus', :colspan => '2') do 'Lunch' end
+        td(:class => 'paus', :colspan => '2') do text 'Lunch' end
       end
       tr do
         td '13:30 - 14:00'
-        td(:class => 'paus', :colspan => '2') do 'Open Space inledning' end
+        td(:class => 'paus', :colspan => '2') do text 'Open Space inledning' end
       end
       tr do
         td '14:00 - 14:45'
-        td(:class => 'paus', :colspan => '2') do 'Open Space' end
+        td(:class => 'paus', :colspan => '2') do text 'Open Space' end
       end
       tr do
         td '15:00 - 15:45'
-        td(:class => 'paus', :colspan => '2') do 'Open Space' end
+        td(:class => 'paus', :colspan => '2') do text 'Open Space' end
       end
       tr do
         td '16:00 - 16:45'
-        td(:class => 'paus', :colspan => '2') do 'Avslutning' end
+        td(:class => 'paus', :colspan => '2') do text 'Avslutning' end
       end
     end
     end
