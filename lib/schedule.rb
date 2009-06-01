56f7f67b21a80b66461e842b4570387d48ad301d
Marcus Ahnve <marcus@ahnve.com>
2009-06-01 21:41:19 +0200

    changed schedule

--- lib/schedule.rb
+++ lib/schedule.rb
@@ -56,11 +56,11 @@ class Schedule
   def setup_monday
     monday.add_slot(MONDAY_SLOTS[0], '49f3145368398b7c1f6ad46efbd950ff', '60164966329af99258739c60ac10123a')
     monday.add_slot(MONDAY_SLOTS[1], '0686eaa4ea9784800c8202420aa5f577', 'a0c860b04d0e3388ef198a5eca483aa3')
-    monday.add_slot(MONDAY_SLOTS[2], '8c98fe235c2800b620c89a9bc4f69afc', 'f9eb28c16fcd36d453ca433f7c9e469d')
+    monday.add_slot(MONDAY_SLOTS[2], '8c98fe235c2800b620c89a9bc4f69afc', 'd1b81518d3b66bb095de29c3faf51893')
     monday.add_slot(MONDAY_SLOTS[3], 'b332e8fc8e1faf8ec4413e0653719fb3', 'dd7774b5277530416037b9e972ff1883')
 
     monday.add_slot(MONDAY_SLOTS[4], 'daf2793559acf601b1fbcc99b6b3cd9b', 'e8cc57326ab9112a116be93a22fa0ec0')
-    monday.add_slot(MONDAY_SLOTS[5], 'd1b81518d3b66bb095de29c3faf51893', '361db5fb16b6580205d7224990827d9f')
+    monday.add_slot(MONDAY_SLOTS[5], 'dd701b51fbb88ef55132342a1e6aa320', '361db5fb16b6580205d7224990827d9f')
     monday.add_slot(MONDAY_SLOTS[6], 'e447ef84faec5ab672d0a2b23106674c', '3e6d5b0a38d99e3d2e738228e6b416b8')
     monday.add_slot(MONDAY_SLOTS[7], 'c2e4d9ff67e581a6d4cd54e5af5d2199', '69580c30faab03bffb1dd24c4ab18927')
     monday.add_slot(MONDAY_SLOTS[8], 'e71a43fca09658b72615e7e24080d536', '6fac4bcc3725ce27a0f79e02a31c4d6d')
@@ -83,7 +83,7 @@ class Schedule
     tuesday.add_slot(TUESDAY_SLOTS[6], '121d8199104d45a8688ee07233b6b499', '192de1197d47658b49c818ea2b0c92f5')
     tuesday.add_slot(TUESDAY_SLOTS[7], 'd983352b62664a69f9edda897108b4ab', '2ccad8aa15cf46909221fd089431fe4a')
     tuesday.add_slot(TUESDAY_SLOTS[8], 'fdec95e03cafedc2ea571354a4cb3286', 'ba7a1fb5c2e513b96e4a61a3162e11e5')
-    tuesday.add_slot(TUESDAY_SLOTS[9], 'dd701b51fbb88ef55132342a1e6aa320', 'e1fe10068ece3b6f33bc3a608eeb5562')
+    tuesday.add_slot(TUESDAY_SLOTS[9], 'f9eb28c16fcd36d453ca433f7c9e469d', 'e1fe10068ece3b6f33bc3a608eeb5562')
 
     tuesday.add_slot(TUESDAY_SLOTS[10], '7384ec11da0d9c152c918c9278f74f61', 'dfcd8940077aa50a2ffa197693826d7a')
     tuesday.add_slot(TUESDAY_SLOTS[11], '0aa645dd565a45cc01536b52a25d8f44', '8fb8e89e94dcd442fabaf8ad3d23b1cb')
