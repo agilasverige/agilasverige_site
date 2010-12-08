81917381a6bb301c4880ce45079db804f99099e5
Marcus Ahnve <marcus@ahnve.com>
2010-12-08 22:31:10 +0100

    move

--- app/models/speaking_proposal.rb
+++ app/models/speaking_proposal.rb
@@ -1,2 +1,18 @@
-class SpeakingProposal < ActiveRecord::Base
+class SpeakingProposal 
+
+  include MongoMapper::Document
+
+  belongs_to :speaker, :class_name => "Attendant", :foreign_key => :attendant_id
+
+  key :title, String, :required => true
+  key :abstract, String, :required => true
+  key :filename, String
+
+
+  def base_file_name
+    title.gsub(/\s/,'_').gsub(/[^a-zA-Z0-9_]/, "")
+  end
+
+
 end
+
