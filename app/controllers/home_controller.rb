1fcd5eb59971d0edf919bc2fc7a21c549a023406
Marcus Ahnve <marcus@ahnve.com>
2010-12-19 22:31:44 +0100

    resourcified home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
 
-  def index
+  def show
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Agilasverige::Application.routes.draw do
     resource :speaking_proposals 
   end
 
+  resource :home
+
   
 
   # The priority is based upon order of creation:
@@ -59,7 +61,7 @@ Agilasverige::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "home#index"
+  root :to => "home#show"
 
   # See how all your routes lay out with "rake routes"
 
