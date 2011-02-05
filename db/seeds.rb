4d596f6c91a83cda9d93b2612963518ee57cb1d0
Marcus Ahnve <marcus@ahnve.com>
2011-02-05 23:56:55 +0100

    seeds an admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
+require File.join(File.dirname(__FILE__),'..','spec','support','blueprints')
+
+u = User.make!(:email => 'admin@example.org', :password => '121212', :password_confirmation => '121212', :role => 'admin')
+u.confirm!
