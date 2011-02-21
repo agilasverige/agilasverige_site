4d303842202a46b931122f37c79562f6dc268ed7
Marcus Ahnve <marcus@ahnve.com>
2011-02-21 22:19:44 +0100

    limit sponsors

--- app/lib/sponsor_list.rb
+++ app/lib/sponsor_list.rb
@@ -17,18 +17,18 @@ class SponsorList
   def setup_sponsors
     @sponsors = [
       Sponsor.new("Agical", "agical_logo.png", "http://www.agical.se"),
-      Sponsor.new("Dynabyte", "dynabyte_logo.png", "http://www.dynabyte.se"),
-      Sponsor.new("Valtech", "valtech_logo.png", "http://www.valtech.se"),
-      Sponsor.new("Agero", "agero_logo.png", "http://www.agero.se"),
+      # Sponsor.new("Dynabyte", "dynabyte_logo.png", "http://www.dynabyte.se"),
+      # Sponsor.new("Valtech", "valtech_logo.png", "http://www.valtech.se"),
+      # Sponsor.new("Agero", "agero_logo.png", "http://www.agero.se"),
       Sponsor.new("GlobalInn", "globalinn_logo.png", "http://www.globalinn.com"),
-      Sponsor.new("Avega", "avega_logo.png", "http://www.avegagroup.se"),
-      Sponsor.new("Jaybis", "jaybis_logo.png", "http://www.jaybis.se"),
-      Sponsor.new("Adaptiv", "adaptiv_logo.png", "http://www.adaptiv.se"),
-      Sponsor.new("Attentec", "attentec_logo.png", "http://www.attentec.se"),
-      Sponsor.new("Citerus", "citerus_logo.png", "http://www.citerus.se"),
+      # Sponsor.new("Avega", "avega_logo.png", "http://www.avegagroup.se"),
+      # Sponsor.new("Jaybis", "jaybis_logo.png", "http://www.jaybis.se"),
+      # Sponsor.new("Adaptiv", "adaptiv_logo.png", "http://www.adaptiv.se"),
+      # Sponsor.new("Attentec", "attentec_logo.png", "http://www.attentec.se"),
+      # Sponsor.new("Citerus", "citerus_logo.png", "http://www.citerus.se"),
       Sponsor.new("Responsive", "responsive_logo.png", "http://www.responsive.se"),
-      Sponsor.new("Omegapoint", "omegapoint_logo.png", "http://www.omegapoint.se"),
-      Sponsor.new("Abiliteam", "abiliteam_logo.png", "http://www.abiliteam.se")
+      # Sponsor.new("Omegapoint", "omegapoint_logo.png", "http://www.omegapoint.se"),
+      # Sponsor.new("Abiliteam", "abiliteam_logo.png", "http://www.abiliteam.se")
       # Sponsor.new("Informator", "informator_logo.png", "http://www.informator.se"),
       # Sponsor.new("Crisp", "crisp_logo.png", "http://www.crisp.se")
     ]
