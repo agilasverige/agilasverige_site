c717ddc2d352ee900c2cb616c6da2d7fcfcf500b
Marcus Ahnve <marcus@ahnve.com>
2011-02-23 22:16:33 +0100

    rvm etc

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,16 @@
+require "bundler/capistrano"
+
+$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
+require "rvm/capistrano"                  # Load RVM's capistrano plugin.
+set :rvm_ruby_string, '1.9.2@agilasverige'        # Or whatever env you want it to run in.
+
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
 
 set :application, "agilasverige" 
 set :user, "agilasverige" 
 set :domain, "agilasverige" 
+set :use_sudo, false
 
 set :port, 22666
 set :deploy_to, "/home/agilasverige/apps/#{application}" 
