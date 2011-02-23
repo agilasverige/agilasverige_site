f00d7b1a894920acac1c85d243906dc747f09270
Marcus Ahnve <marcus@ahnve.com>
2011-02-23 22:54:43 +0100

    prod env updated for email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,6 +36,9 @@ Agilasverige::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => 'agilasverige.se' }
+  config.action_mailer.delivery_method = :sendmail
+
 
   # Enable threaded mode
   # config.threadsafe!
