9361841ee6602640cf7c540f0d3e6205197f02d5
Marcus Ahnve <marcus@ahnve.com>
2011-02-23 22:45:30 +0100

    cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
-require "bundler/capistrano"
-
 $:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
 require "rvm/capistrano"                  # Load RVM's capistrano plugin.
 set :rvm_ruby_string, '1.9.2@agilasverige'        # Or whatever env you want it to run in.
+
+require "bundler/capistrano"
 
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
@@ -30,4 +30,12 @@ namespace :deploy do
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
+
+  desc "Symlink shared configs and folders on each release."
+  task :symlink_shared do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    run "ln -nfs #{shared_path}/files #{release_path}/public/files"
+  end
 end
+
+after 'deploy:update_code', 'deploy:symlink_shared'
