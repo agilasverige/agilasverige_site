1522114eb110c6177d0cf6675b711c0cb06357ce
Marcus Ahnve <marcus@ahnve.com>
2011-02-24 11:49:26 +0100

    adaptiv in da spons house

--- spec/lib/sponsor_list_spec.rb
+++ spec/lib/sponsor_list_spec.rb
@@ -11,6 +11,7 @@ describe "SponsorList" do
     sponsors = [['GlobalInn','globalinn.com'],
                 ['Agical','agical.se'],
                 ['Dynabyte','dynabyte.se'],
+                ['Adaptiv','adaptiv.se'],
                 ['Responsive', 'responsive.se']]
 
     sponsors.each do |sponsor|
@@ -34,8 +35,8 @@ describe "a random sponsorlist" do
     @random_ordered_sponsors = @sponsor_list.random_order
   end
 
-  it "should have 4 elements" do
-    @random_ordered_sponsors.length.should == 4
+  it "should have 5 elements" do
+    @random_ordered_sponsors.length.should == 5
   end
   
   it "should not provide the list ordered the same way twice" do
