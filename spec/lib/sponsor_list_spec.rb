504107d031ca262577627215870f2d3c36ce771c
Marcus Ahnve <marcus@ahnve.com>
2011-03-24 22:55:51 +0100

    jaybis sponsor

--- spec/lib/sponsor_list_spec.rb
+++ spec/lib/sponsor_list_spec.rb
@@ -15,6 +15,7 @@ describe "SponsorList" do
                 ['Valtech','valtech.se'],
                 ['Omegapoint','omegapoint.se'],
                 ['Citerus','citerus.se'],
+                ['Jaybis','jaybis.se'],
                 ['Agero','agero.se'],
                 ['Adaptiv','adaptiv.se'],
                 ['AddQ','addq.se'],
@@ -41,8 +42,8 @@ describe "a random sponsorlist" do
     @random_ordered_sponsors = @sponsor_list.random_order
   end
 
-  it "should have 11 elements" do
-    @random_ordered_sponsors.length.should == 11
+  it "should have 12 elements" do
+    @random_ordered_sponsors.length.should == 12
   end
   
   it "should not provide the list ordered the same way twice" do
