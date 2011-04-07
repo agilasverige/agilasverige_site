2a5e5f96837acefffbb0b6d767f6dfd88eaa17c3
Marcus Ahnve <marcus@ahnve.com>
2011-04-07 16:25:29 +0200

    stopping wo internet

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,19 @@ class UsersController < InheritedResources::Base
 
   before_filter :authenticate_user!
   before_filter :setup_user
+  before_filter :authorize_admin!
 
   private
 
   def setup_user
     if !current_user.admin? || params['id'].blank?
       @user = current_user
+    end
+  end
+
+  def authorize_admin!
+    unless current_user.admin?
+      render :file => 'public/404.html', :status => 404
     end
   end
   
