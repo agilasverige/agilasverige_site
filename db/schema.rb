8f31d57e49e9c95b64a86b468c4a88b6659908df
Marcus Ahnve <marcus@ahnve.com>
2011-05-26 11:48:56 +0200

    rails admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,25 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110205211236) do
+ActiveRecord::Schema.define(:version => 20110525144038) do
 
   create_table "editable_content", :force => true do |t|
     t.text   "content"
     t.string "page_name"
   end
+
+  create_table "rails_admin_histories", :force => true do |t|
+    t.string   "message"
+    t.string   "username"
+    t.integer  "item"
+    t.string   "table"
+    t.integer  "month",      :limit => 2
+    t.integer  "year",       :limit => 8
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "rails_admin_histories", ["item", "table", "month", "year"], :name => "index_histories_on_item_and_table_and_month_and_year"
 
   create_table "speaking_engagements", :force => true do |t|
     t.integer  "user_id"
