5961a1c58483a1ec35da899d1073ab66bc9ab6f7
Marcus Ahnve <marcus@ahnve.com>
2012-01-18 21:26:47 +0100

    removed old migrations from admin that did not really fit

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,81 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110525144038) do
+ActiveRecord::Schema.define(:version => 20111229124756) do
 
   create_table "editable_content", :force => true do |t|
     t.text   "content"
     t.string "page_name"
   end
+
+  create_table "images", :force => true do |t|
+    t.string   "image_mime_type"
+    t.string   "image_name"
+    t.integer  "image_size"
+    t.integer  "image_width"
+    t.integer  "image_height"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "image_uid"
+    t.string   "image_ext"
+  end
+
+  create_table "page_part_translations", :force => true do |t|
+    t.integer  "page_part_id"
+    t.string   "locale"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "page_part_translations", ["page_part_id"], :name => "index_page_part_translations_on_page_part_id"
+
+  create_table "page_parts", :force => true do |t|
+    t.integer  "page_id"
+    t.string   "title"
+    t.text     "body"
+    t.integer  "position"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "page_parts", ["id"], :name => "index_page_parts_on_id"
+  add_index "page_parts", ["page_id"], :name => "index_page_parts_on_page_id"
+
+  create_table "page_translations", :force => true do |t|
+    t.integer  "page_id"
+    t.string   "locale"
+    t.string   "title"
+    t.string   "custom_title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "page_translations", ["page_id"], :name => "index_page_translations_on_page_id"
+
+  create_table "pages", :force => true do |t|
+    t.integer  "parent_id"
+    t.integer  "position"
+    t.string   "path"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.boolean  "show_in_menu",        :default => true
+    t.string   "link_url"
+    t.string   "menu_match"
+    t.boolean  "deletable",           :default => true
+    t.string   "custom_title_type",   :default => "none"
+    t.boolean  "draft",               :default => false
+    t.boolean  "skip_to_first_child", :default => false
+    t.integer  "lft"
+    t.integer  "rgt"
+    t.integer  "depth"
+  end
+
+  add_index "pages", ["depth"], :name => "index_pages_on_depth"
+  add_index "pages", ["id"], :name => "index_pages_on_id"
+  add_index "pages", ["lft"], :name => "index_pages_on_lft"
+  add_index "pages", ["parent_id"], :name => "index_pages_on_parent_id"
+  add_index "pages", ["rgt"], :name => "index_pages_on_rgt"
 
   create_table "rails_admin_histories", :force => true do |t|
     t.string   "message"
@@ -29,6 +98,69 @@ ActiveRecord::Schema.define(:version => 20110525144038) do
   end
 
   add_index "rails_admin_histories", ["item", "table", "month", "year"], :name => "index_histories_on_item_and_table_and_month_and_year"
+
+  create_table "refinery_settings", :force => true do |t|
+    t.string   "name"
+    t.text     "value"
+    t.boolean  "destroyable",             :default => true
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "scoping"
+    t.boolean  "restricted",              :default => false
+    t.string   "callback_proc_as_string"
+    t.string   "form_value_type"
+  end
+
+  add_index "refinery_settings", ["name"], :name => "index_refinery_settings_on_name"
+
+  create_table "resources", :force => true do |t|
+    t.string   "file_mime_type"
+    t.string   "file_name"
+    t.integer  "file_size"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "file_uid"
+    t.string   "file_ext"
+  end
+
+  create_table "roles", :force => true do |t|
+    t.string "title"
+  end
+
+  create_table "roles_users", :id => false, :force => true do |t|
+    t.integer "user_id"
+    t.integer "role_id"
+  end
+
+  add_index "roles_users", ["role_id", "user_id"], :name => "index_roles_users_on_role_id_and_user_id"
+  add_index "roles_users", ["user_id", "role_id"], :name => "index_roles_users_on_user_id_and_role_id"
+
+  create_table "seo_meta", :force => true do |t|
+    t.integer  "seo_meta_id"
+    t.string   "seo_meta_type"
+    t.string   "browser_title"
+    t.string   "meta_keywords"
+    t.text     "meta_description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "seo_meta", ["id"], :name => "index_seo_meta_on_id"
+  add_index "seo_meta", ["seo_meta_id", "seo_meta_type"], :name => "index_seo_meta_on_seo_meta_id_and_seo_meta_type"
+
+  create_table "slugs", :force => true do |t|
+    t.string   "name"
+    t.integer  "sluggable_id"
+    t.integer  "sequence",                     :default => 1, :null => false
+    t.string   "sluggable_type", :limit => 40
+    t.string   "scope",          :limit => 40
+    t.datetime "created_at"
+    t.string   "locale"
+  end
+
+  add_index "slugs", ["locale"], :name => "index_slugs_on_locale"
+  add_index "slugs", ["name", "sluggable_type", "scope", "sequence"], :name => "index_slugs_on_n_s_s_and_s", :unique => true
+  add_index "slugs", ["sluggable_id"], :name => "index_slugs_on_sluggable_id"
 
   create_table "speaking_engagements", :force => true do |t|
     t.integer  "user_id"
@@ -44,42 +176,33 @@ ActiveRecord::Schema.define(:version => 20110525144038) do
     t.datetime "updated_at"
   end
 
+  create_table "user_plugins", :force => true do |t|
+    t.integer "user_id"
+    t.string  "name"
+    t.integer "position"
+  end
+
+  add_index "user_plugins", ["name"], :name => "index_user_plugins_on_title"
+  add_index "user_plugins", ["user_id", "name"], :name => "index_unique_user_plugins", :unique => true
+
   create_table "users", :force => true do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "organization"
-    t.string   "address"
-    t.string   "zip_code"
-    t.string   "postal_address"
-    t.string   "country"
-    t.string   "invoice_reference"
-    t.string   "telephone_number"
-    t.boolean  "attending_dinner"
-    t.string   "food_preferences"
-    t.text     "comments"
-    t.boolean  "unregistered"
-    t.string   "role"
+    t.string   "username",             :null => false
+    t.string   "email",                :null => false
+    t.string   "encrypted_password",   :null => false
+    t.string   "persistence_token"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "email",                               :default => "", :null => false
-    t.string   "encrypted_password",   :limit => 128, :default => "", :null => false
-    t.string   "password_salt",                       :default => "", :null => false
-    t.string   "reset_password_token"
-    t.string   "remember_token"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                       :default => 0
+    t.string   "perishable_token"
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.string   "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string   "authentication_token"
+    t.integer  "sign_in_count"
+    t.string   "remember_token"
+    t.string   "reset_password_token"
+    t.datetime "remember_created_at"
   end
 
-  add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+  add_index "users", ["id"], :name => "index_users_on_id"
 
 end
