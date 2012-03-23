43182e60760819ec462c6a8c935099307861e2e8
Jimmy Larsson <jimmy.k.larsson@gmail.com>
2012-03-23 15:35:01 +0100

    undoing last commit

--- config/application.rb
+++ config/application.rb
@@ -6,8 +6,6 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env) if defined?(Bundler)
 
-ActiveRecord::Base.partial_updates = false
-
 module Agilasverige
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
