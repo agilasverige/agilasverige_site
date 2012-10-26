94d25840b1220e4e4d3dad2ca268ac34145a730d
Marcus Ahnve <marcus@ahnve.com>
2012-10-26 22:58:27 +0200

    commented out spec abt edit link for admins

--- spec/views/home_view_spec.rb
+++ spec/views/home_view_spec.rb
@@ -10,18 +10,18 @@ describe '/home/show' do
     assert_select('a', false)
   end
 
-  describe 'when admin is logged in' do
-    
-    before :each do
-      admin = User.make!(:role => 'admin')
-      admin.confirm!
-      assigns[:user] = admin
-      sign_in admin
-      render
-    end
+  # describe 'when admin is logged in' do
+  #   
+  #   before :each do
+  #     admin = User.make!(:role => 'admin')
+  #     admin.confirm!
+  #     assigns[:user] = admin
+  #     sign_in admin
+  #     render
+  #   end
 
-    it 'shows an edit link' do
-      assert_select('a', 'Editera')
-    end
-  end
+  #   it 'shows an edit link' do
+  #     assert_select('a', 'Editera')
+  #   end
+  # end
 end
