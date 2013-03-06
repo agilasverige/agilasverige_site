b7a1378c7712e86791cc98f92e2e4634b05f04ce
Jimmy Larsson <jimmy.k.larsson@gmail.com>
2013-03-06 16:58:54 +0100

    added mappings of the payson statuses

--- app/lib/payson_client.rb
+++ app/lib/payson_client.rb
@@ -1,5 +1,17 @@
 module PaysonClient
 
+  PAYSON_STATUSES = {
+      "CREATED" => :paid,         # The payment request was received and has been created in Paysons system. Funds will be transferred once approval is received.
+      "PENDING" => :paid,         # The sender has a pending transaction. A guarantee payment in progress has status pending. Please check guaranteeStatus for further details.
+      "PROCESSING" => :paid,      # The payment is in progress, check again later.
+      "COMPLETED" => :paid,       # The senders transaction has completed.
+      "CREDITED"  => :failed,     # The senders transaction has been credited.
+      "ERROR" => :failed,         # The payment failed and all attempted transfers failed or all completed transfers were successfully reversed.
+      "EXPIRED" => :failed,       # A payment requiring approval was not executed within 3 hours.
+      "REVERSALERROR" => :failed, # One or more transfers failed when attempting to reverse a payment.
+      "ABORTED" => :failed        # The payment was aborted before any money were transferred.
+  }
+
   def self.load_registration_status(user)
     return :not_logged_in unless user
 
@@ -13,15 +25,8 @@ module PaysonClient
       payment_details = PaysonAPI::Request::PaymentDetails.new(token)
       response = PaysonAPI::Client.get_payment_details(payment_details)
 
-      status = nil
-      if response.success?
-        # TODO: map these
-        return response.status.to_sym
-      else
-        return :unknown
-      end
+      return PAYSON_STATUSES.fetch(response.status, :unknown)
     end
     return :not_started
   end
-
 end
