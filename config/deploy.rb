25b7725ae082988b8d9658aed42db0eb6cf5af7c
Jimmy Larsson <jimmy.k.larsson@gmail.com>
2013-05-15 21:30:08 +0200

    messing

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,4 @@
-$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
+#$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
 require "rvm/capistrano"                  # Load RVM's capistrano plugin.
 set :rvm_ruby_string, '1.9.2@agilasverige'        # Or whatever env you want it to run in.
 
