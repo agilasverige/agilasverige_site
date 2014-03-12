16bdf89897bbe5b50148faf3af8587bad939005c
Marcus Ahnve <marcus@ahnve.com>
2014-03-12 09:12:53 +0100

    loggor, loggor, loggor

--- app/lib/sponsor_list.rb
+++ app/lib/sponsor_list.rb
@@ -16,12 +16,12 @@ class SponsorList
 
   def default_sponsors
     [
-      #Sponsor.new("Agical", "agical_logo.png", "http://www.agical.se"),
+      Sponsor.new("Agical", "agical_logo.png", "http://www.agical.se"),
       Sponsor.new("Dynabyte", "dynabyte_logo.jpg", "http://www.dynabyte.se"),
       # Sponsor.new("AddQ", "addq_logo.png", "http://www.addq.se"),
       Sponsor.new("Valtech", "valtech_logo.png", "http://www.valtech.se"),
       # Sponsor.new("Agero", "agero_logo.png", "http://www.agero.se"),
-      #Sponsor.new("Solidtango", "solidtango_logo.png", "http://www.solidtango.com"),
+      Sponsor.new("Solidtango", "solidtango_logo.png", "http://www.solidtango.com"),
       #Sponsor.new("Avega", "avega_logo.png", "http://www.avegagroup.se"),
       # Sponsor.new("Jaybis", "jaybis_logo.png", "http://www.jaybis.se"),
       Sponsor.new("Adaptiv", "adaptiv_logo.png", "http://www.adaptiv.se"),
@@ -34,6 +34,7 @@ class SponsorList
       # Sponsor.new("Abiliteam", "abiliteam_logo.png", "http://www.abiliteam.se")
       Sponsor.new("Informator", "informator_logo.png", "http://www.informator.se"),
       Sponsor.new("Crisp", "crisp_logo.png", "http://www.crisp.se"),
+      Sponsor.new("King", "king_logo.png", "http://www.king.com")
       # Sponsor.new("Know IT", "knowit_logo.png", "http://www.knowit.se"),
       #Sponsor.new("Spotify", "spotify_logo.png", "http://www.spotify.se")
       # Sponsor.new("Scania", "scania_logo.gif", "http://www.scania.se"),
