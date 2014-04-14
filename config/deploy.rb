2a0346577367b899f7cb471c84061b500f601610
Marcus Ahnve <marcus@ahnve.com>
2014-04-14 19:58:13 +0200

    live crazy bug fixing coding

--- config/deploy.rb
+++ config/deploy.rb
@@ -56,16 +56,14 @@ task :console, :roles => :app do
   end
 end
 
-task :notify_ratchetio, :roles => :app do
-  set :revision, `git log -n 1 --pretty=format:"%H"`
-  set :local_user, `whoami`
-  set :ratchetio_token, '7f7e772321a748e4b5c470b5aec54355'
-  rails_env = fetch(:rails_env, 'production')
-  run "curl https://submit.ratchet.io/api/1/deploy/ -F access_token=#{ratchetio_token} -F environment=#{rails_env} -F revision=#{revision} -F local_username=#{local_user}"
-end
+require 'rollbar/capistrano'
+set :rollbar_token, '7f7e772321a748e4b5c470b5aec54355'
 
+after "deploy", "newrelic:notice_deployment"
 after "deploy:update", "newrelic:notice_deployment"
-after :deploy, 'notify_ratchetio'
+after "deploy:migrations", "newrelic:notice_deployment"
+after "deploy:cold", "newrelic:notice_deployment"
+
 after 'deploy:update_code', 'deploy:symlink_shared'
 
 # Clean out old releases
