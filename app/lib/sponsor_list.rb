8ab165e026ce86a24331d3541caaee683f3c371a
Ulrika Malmgren <ulrika.malmgren@magine.com>
2015-05-14 19:33:20 +0200

    Kings logga

--- app/lib/sponsor_list.rb
+++ app/lib/sponsor_list.rb
@@ -25,8 +25,8 @@ class SponsorList
 #      Sponsor.new("Adaptiv", "adaptiv_logo.png", "http://www.adaptiv.se"),
       Sponsor.new("Citerus", "citerus_logo.png", "http://www.citerus.se"),
       Sponsor.new("Informator", "informator_logo.png", "http://informator.se/utbildningar/agila-metoder"),
-       Sponsor.new("Crisp", "crisp_logo_liggande.png", "http://www.crisp.se"),
-#      Sponsor.new("King", "king_logo.png", "http://www.king.com"),
+      Sponsor.new("Crisp", "crisp_logo_liggande.png", "http://www.crisp.se"),
+      Sponsor.new("King", "king_logo.png", "http://www.king.com"),
 #      Sponsor.new("Claremont", "claremont_logo.jpg", "http://www.claremont.se"),
 #      Sponsor.new("TriOptima", "trioptima_logo.jpg", "http://www.trioptima.com"),
 #      Sponsor.new("Mynewsdesk", "mynewsdesk_logo.png", "http://www.mynewsdesk.com")
@@ -37,14 +37,14 @@ class SponsorList
       # Sponsor.new("Attentec", "attentec_logo.png", "http://www.attentec.se"),
       #Sponsor.new("Responsive", "responsive_logo.png", "http://www.responsive.se"),
       # Sponsor.new("CAG", "cag_logo.png", "http://www.cag.se"),
-        Sponsor.new("Omegapoint", "omegapoint_logo.png", "http://www.omegapoint.se"),
+      Sponsor.new("Omegapoint", "omegapoint_logo.png", "http://www.omegapoint.se"),
       # Sponsor.new("Abiliteam", "abiliteam_logo.png", "http://www.abiliteam.se")
       # Sponsor.new("AddQ", "addq_logo.png", "http://www.addq.se"),
       # Sponsor.new("Know IT", "knowit_logo.png", "http://www.knowit.se"),
       # Sponsor.new("Spotify", "spotify_logo.png", "http://www.spotify.se")
       # Sponsor.new("Scania", "scania_logo.gif", "http://www.scania.se"),
-       Sponsor.new("Inceptive", "inceptive_logo.jpg", "http://www.inceptivestockholm.se"),
-       Sponsor.new("Transmode", "TRM_logo_green-blue_.jpg", "http://www.transmode.com"),
+      Sponsor.new("Inceptive", "inceptive_logo.jpg", "http://www.inceptivestockholm.se"),
+      Sponsor.new("Transmode", "TRM_logo_green-blue_.jpg", "http://www.transmode.com"),
       Sponsor.new("Avidity", "avidity_logo.png", "http://www.avidity.se"),
       Sponsor.new("Kambi", "kambi_logo.jpg", "http://www.kambi.com")
     ]
